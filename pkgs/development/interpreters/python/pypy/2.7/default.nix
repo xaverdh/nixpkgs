c1b05733ac20a06e8fb45e69aad259dd4c18bc59
Frederik Rietdijk <fridh@fridh.nl>
2017-05-28 09:20:47 +0200

    Python: the pythonModule attribute
    Python libraries or modules now have an attribute `pythonModule = interpreter;` to indicate
    they provide Python modules for the specified `interpreter`.
    
    The package set provides the following helper functions:
    
    - hasPythonModule: Check whether a derivation provides a Python module.
    - requiredPythonModules: Recurse into a list of Python modules, returning all Python modules that are required.
    - makePythonPath: Create a PYTHONPATH from a list of Python modules.
    
    Also included in this commit is:
    - disabledIf: Helper function for disabling non-buildPythonPackage functions.

--- pkgs/development/interpreters/python/pypy/2.7/default.nix
+++ pkgs/development/interpreters/python/pypy/2.7/default.nix
@@ -129,7 +129,7 @@ in stdenv.mkDerivation rec {
       inherit zlibSupport libPrefix sitePackages;
       executable = "pypy";
       isPypy = true;
-      buildEnv = callPackage ../../wrapper.nix { python = self; };
+      buildEnv = callPackage ../../wrapper.nix { python = self; inherit (pythonPackages) requiredPythonModules; };
       interpreter = "${self}/bin/${executable}";
       withPackages = import ../../with-packages.nix { inherit buildEnv pythonPackages;};
       pkgs = pythonPackages;
