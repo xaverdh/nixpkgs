3ef1d091d375f8ee1380772d2ccc0e68ba18fa5a
Ingo Blechschmidt <iblech@speicherleck.de>
2019-03-08 12:49:25 +0100

    xteddy: fix usage text to reference the correct manpage

--- pkgs/applications/misc/xteddy/default.nix
+++ pkgs/applications/misc/xteddy/default.nix
@@ -4,12 +4,15 @@ stdenv.mkDerivation rec {
   name = "xteddy-${version}";
   version = "2.2";
   src = fetchzip {
-    url = "http://deb.debian.org/debian/pool/main/x/xteddy/xteddy_${version}.orig.tar.gz";
+    url = "https://deb.debian.org/debian/pool/main/x/xteddy/xteddy_${version}.orig.tar.gz";
     sha256 = "0sap4fqvs0888ymf5ga10p3n7n5kr35j38kfsfd7nj0xm4hmcma3";
   };
   nativeBuildInputs = [ pkg-config makeWrapper ];
   buildInputs = [ imlib2 xorg.libX11 xorg.libXext ];
   makeFlags = [ "LIBS=-lXext" ];
+  preBuild = ''
+    sed -i 's/man 1 xteddy/man 6 xteddy/' xteddy.c
+  '';
   postInstall = ''
     cp -R images $out/share/images
     # remove broken scripts
@@ -20,7 +23,7 @@ stdenv.mkDerivation rec {
   '';
   meta = with stdenv.lib; {
     description = "cuddly teddy bear for your X desktop";
-    homepage = http://weber.itn.liu.se/~stegu/xteddy/;
+    homepage = https://weber.itn.liu.se/~stegu/xteddy/;
     license = licenses.gpl2;
     maintainers = [ maintainers.xaverdh ];
     platforms = platforms.linux;
